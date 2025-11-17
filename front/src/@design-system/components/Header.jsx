fda527f54d2270c553da9a79d580b61ca441b292
jina <rkdwlsk94@gmail.com>
2025-11-17 13:29:07 +0900

    랜덤 응원

--- front/src/@design-system/components/Header.jsx
+++ front/src/@design-system/components/Header.jsx
@@ -27,34 +27,34 @@ export const Header = memo(() => {
   };
 
   const pillBtn =
-    "no-underline font-[DungeonFighterOnlineBeatBeat] text-[#FFFFFF] " +
+    "text-2xl text-stroke-2 font-[DungeonFighterOnlineBeatBeat] text-[#FFFFFF] " +
     "inline-flex items-center justify-center " +
     "w-[130.7px] h-[57.3px] shrink-0 " +
     "rounded-full border-[2.5px] border-[#6C798A] bg-[#B1D2FA] " +
     "leading-none select-none transition-all duration-200 " +
-    "hover:bg-[#c9ddff] focus:outline-none focus:ring-2 focus:ring-[#B1D2FA]/50";
+    "hover:bg-[#c9ddff] focus:outline-none focus:ring-2 focus:ring-[#B1D2FA]/50 ";
 
   return (
     <header className="sticky z-10 bg-header border-[3px] rounded-[18px] border-[#5E5A6A] shadow-sm">
       <div className="relative w-full max-w-screen-2xl mx-auto px-1 py-1 flex items-center justify-center">
         
         {/* 중앙 로고 */}
-        <Link to="/"  className="flex justify-center items-center">
+        <Link to="/"  className="flex justify-center items-center ">
           <AppTitle><img src = "/rogo2.png" alt ="두근두근지구말" className="h-40 object-contain mx-auto pointer-events-none"/> </AppTitle>
           
         </Link>
         {/* 오른쪽 버튼 고정 */}
         <nav className="absolute font-[DungeonFighterOnlineBeatBeat] right-8 top-1/2 -translate-y-1/2 flex gap-3 z-10">
           {!isAuthenticated ? (
-            <button onClick={handleLoginClick} className={`${pillBtn} flex items-center gap-2`}>
-              <LogIn className="w-5 h-5 " />
-              로그인
+            <button onClick={handleLoginClick} className={`${pillBtn} flex items-center gap-2  text-center`}>
+              {/* <LogIn className="w-5 h-5 " /> */}
+              <span className="login-outline-text" data-label="로그인">로그인</span>
             </button>
 
           ) : (
             <>
               <Link to="/mypage"
-                className="no-underline font-extrabold text-[#394b69]
+                className="text-2xl text-stroke-2 font-[DungeonFighterOnlineBeatBeat] text-[#FFFFFF]
               inline-flex items-center justify-center gap-2
               w-[130.7px] h-[57.3px] shrink-0
               rounded-full border-[2.5px] border-[#6C798A] bg-[#B1D2FA]
@@ -62,15 +62,21 @@ export const Header = memo(() => {
               transition-colors duration-150
               hover:bg-[#c9ddff] focus:outline-none focus:ring-2 focus:ring-[#B1D2FA]/50"
               >
-                <User className="w-5 h-5" />
-                마이페이지
+                {/* <User className="w-5 h-5" /> */}
+                <span className="text-center">마이페이지</span>
               </Link>
               <button
                 onClick={handleLogout}
-                className="no-underline font-bold rounded-full px-5 py-2 border-2 border-[#a89a77] bg-white/70 text-[#394b69] cursor-pointer hover:bg-white transition-colors duration-150 flex items-center gap-2"
+                className="text-2xl text-stroke-2 font-[DungeonFighterOnlineBeatBeat] text-[#FFFFFF]
+              inline-flex items-center justify-center gap-2
+              w-[130.7px] h-[57.3px] shrink-0
+              rounded-full border-[2.5px] border-[#6C798A] bg-[#B1D2FA]
+              leading-none select-none
+              transition-colors duration-150
+              hover:bg-[#c9ddff] focus:outline-none focus:ring-2 focus:ring-[#B1D2FA]/50"
               >
-                <LogOut className="w-4 h-4" />
-                로그아웃
+                {/* <LogOut className="w-4 h-4" /> */}
+                <span className="text-center">로그아웃</span>
               </button>
             </>
           )}
